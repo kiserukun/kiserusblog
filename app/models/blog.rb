3791f0a6d13fe5870133b0dee7e503fd42352b64
kiserukun <73691576+kiserukun@users.noreply.github.com>
2021-02-15 07:50:05 +0900

    アソシエーション

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -6,4 +6,6 @@ class Blog < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_many :comments
+  has_many :tags,through: :blog_tag_relations
+  has_many :blog_tag_relations
 end
