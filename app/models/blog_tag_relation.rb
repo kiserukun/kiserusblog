f7a6866b4ae1b6c5ad9b71b6ad29deb7bedcc880
kiserukun <73691576+kiserukun@users.noreply.github.com>
2021-02-16 09:37:28 +0900

    Update blog_tag_relation.rb

--- app/models/blog_tag_relation.rb
+++ app/models/blog_tag_relation.rb
@@ -1,4 +1,21 @@
-class BlogTagRelation < ApplicationRecord
+class BlogTag
+
+    include ActiveModel::Model
+    attr_accessor :message, :name
+  
+    with_options presence: true do
+      validates :message
+      validates :name
+    end
+  
+    def save
+      tweet = Tweet.create(message: message)
+      tag = Tag.create(name: name)
+  
+      TweetTagRelation.create(tweet_id: tweet.id, tag_id: tag.id)
+    end
+  
+  end
   belongs_to :blog
   belongs_to :tag
 end
